87b2025546331dd19ea4b3fdbea7b7b6cdd4025b
underscore830 <70597619+underscore830@users.noreply.github.com>
2022-09-13 18:54:39 -0400

    player movement added

--- MartialLawless/Assets/Scripts/PlayerController.cs
+++ MartialLawless/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,24 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction playerControls;
+    private Vector2 direction = Vector2.zero;
+    private Vector3 velocity = Vector2.zero;
+    private Vector3 position;
+    public int moveSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        position = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = playerControls.ReadValue<Vector2>();
+
+        velocity = new Vector3(direction.x * moveSpeed, direction.y * moveSpeed, 0);
+        position += velocity * Time.deltaTime;
+        transform.position = position;
     }
 
     private void OnEnable()
