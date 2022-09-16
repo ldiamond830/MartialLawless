b1dc5beb1909423ea98aaa2aafe3ad1b88192de8
Lucas Diamond (RIT Student) <lgd1649@rit.edu>
2022-09-16 12:49:00 -0400

    added spawn points and start of enemy spawning algorithm

--- MartialLawless/Assets/Scripts/EnemyAI.cs
+++ MartialLawless/Assets/Scripts/EnemyAI.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class EnemyAI : MonoBehaviour
 {
+    private Orientation orientation;
+
     [SerializeField]
     private Transform playerTransform;
 
     private float moveSpeed = 2.0f;
 
+    private Vector3 position;
+
+    public Vector3 Position{
+        get{return position;}
+        set{position = value;}
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        orientation = Orientation.up;
         
     }
 
@@ -23,8 +35,33 @@ public class EnemyAI : MonoBehaviour
         // Get the vector from this enemy to the player
         Vector2 moveVector = playerPosition - (Vector2)transform.position;
 
+        moveVector = moveVector.normalized;
+
+         if (moveVector.y > 0)
+        {
+            orientation = Orientation.up;
+        }
+        else if (moveVector.y < 0)
+        {
+
+            orientation = Orientation.down;
+        }
+        else if (moveVector.x > 0)
+        {
+
+            orientation = Orientation.right;
+        }
+        else if (moveVector.x < 0)
+        {
+            orientation = Orientation.left;
+        }
+
+
+
         // Apply the movement to the enemy's transform
-        transform.position += (Vector3)(moveVector.normalized * moveSpeed * Time.deltaTime);
+        position += (Vector3)(moveVector.normalized * moveSpeed * Time.deltaTime);
+
+        transform.position = position;
 
     }
 }
--- MartialLawless/Assets/Scripts/Manager.cs
+++ MartialLawless/Assets/Scripts/Manager.cs
@@ -11,6 +11,13 @@ public class Manager : MonoBehaviour
 
     //when set to true spawns new wave of enemies, when set to false wave is in progress
     private bool isSpawning;
+    private List<EnemyAI> enemyList;
+    public EnemyAI enemyPrefab;
+
+    public GameObject topSpawn;
+    public GameObject bottomSpawn;
+    public GameObject leftSpawn;
+    public GameObject rightSpawn;
 
 
     // Start is called before the first frame update
@@ -19,13 +26,34 @@ public class Manager : MonoBehaviour
         timeBetweenSpawn = 0.2f;
         waveCount = 0;
         isSpawning = true;
+        enemyList = new List<EnemyAI>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(isSpawning){
+            
+            for(int i = 0; i < waveCount; i++){
+                EnemyAI newEnemy = Instantiate(enemyPrefab);
 
+                 enemyList.Add(newEnemy);
+                 
+                 int doorSelect = Random.Range(0,4);
+
+                 if(doorSelect == 0){
+                    newEnemy.Position = topSpawn.transform.position;
+                 }
+                 else if(doorSelect == 1){
+                    newEnemy.Position = rightSpawn.transform.position;
+                 }
+                 else if(doorSelect == 2){
+                    newEnemy.Position = leftSpawn.transform.position;
+                 }
+                 else{
+                    newEnemy.Position = rightSpawn.transform.position;
+                 }
+            }
         }
         else{
 
